f57b5a9b7810120b80aa26ae8a49042966d9741e
anggerwicaksono <77134048+anggerwicaksono@users.noreply.github.com>
2021-01-17 17:58:34 +0700

    Update car_brand_classifier_yolo4.py

--- car_brand_classifier_yolo4.py
+++ car_brand_classifier_yolo4.py
@@ -25,11 +25,17 @@ tres = st.sidebar.selectbox('Treshold Value (default 0.3)', values, index=def_tr
 def main():
     # Render the readme as markdown using st.markdown.
     st.write("""
-    # CCD Car Color Detection
-    Implementation of AI Object Detection using YOLOv4 (OpenCV2 DNN and MNN backend) on Streamlit
+    # CCD Car Make and Color Detection
+    Implementation of AI Object Detection using YOLOv4
+    
+    (OpenCV2 DNN and MNN backend) on Streamlit
+    
     (code forked from :
+    
     https://github.com/spectrico/car-color-classifier-yolo4-python
+    
     https://github.com/spectrico/vehicle-brand-recognition-yolov4-python)
+    
     """)
 
     # Download external dependencies.
